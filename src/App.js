7e54372a90f1daf1feeb1cc2ca7dfa4985b91d70
Awais-Ansarii <awaisansari1502@gmail.com>
2024-01-26 22:20:49 +0530

    added dark light theme mode functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-
+import { AppContext } from "./utils/AppContext";
+import { useContext } from "react";
 import MainContainer from "./components/MainContainer";
+
 function App() {
-  return (<MainContainer/>)
+  const { theme } = useContext(AppContext);
+  return (
+    <div
+      className={` full-height ${
+        theme ? "bg-black text-white " : "bg-white text-dark "
+      } `}
+      style={{ minHeight: "100vh" }}
+    >
+      <MainContainer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,17 +2,33 @@ import React from "react";
 import { AppContext } from "../utils/AppContext";
 import { useContext } from "react";
 import Form from "./Form";
+import Button from "react-bootstrap/Button";
 
 const MainContainer = () => {
-  const { theme, setTheme, combinator, setCombinator, rules, setRules } =
-    useContext(AppContext);
+  const { theme, setTheme, combinator, rules } = useContext(AppContext);
   return (
-    <div className="container d-flex flex-column align-items-center mb-3">
-      <h1 className=" mt-3 mb-2">Expression Engine UI</h1>
+    <div className={` d-flex flex-column align-items-center mb-3  `}>
+      <div className="  mt-3 mb-2 d-flex justify-content-center gap-5 align-items-center">
+        <h1 className=" ">Expression Engine UI</h1>
+        {theme ? (
+          <Button onClick={() => setTheme(!theme)} variant="light">
+            Light-Theme🌙
+          </Button>
+        ) : (
+          <Button onClick={() => setTheme(!theme)} variant="dark">
+            Dark-Theme🌙
+          </Button>
+        )}
+      </div>
 
       <Form />
       <hr />
-      <div className="w-50 p-3 bg-primary-subtle rounded">
+      <div
+        className={`w-50 p-3 mb-4 rounded ${
+          theme ? "bg-secondary text-white" : "bg-success-subtle"
+          }`}
+        
+      >
         <pre className="w-50 mx-auto fw-semibold">
           {JSON.stringify({ rules, combinator }, null, 2)}
         </pre>
